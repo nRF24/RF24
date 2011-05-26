40eea86ffe6993eecb3ef97652009821f5b316a7
maniacbug <maniacbug@ymail.com>
2011-05-25 20:46:31 -0700

    Avoid spurious warnings

--- RF24.cpp
+++ RF24.cpp
@@ -18,6 +18,12 @@
 #define IF_SERIAL_DEBUG(x)
 #endif
 
+// Avoid spurious warnings
+#undef PROGMEM 
+#define PROGMEM __attribute__(( section(".progmem.data") )) 
+#undef PSTR 
+#define PSTR(s) (__extension__({static prog_char __c[] PROGMEM = (s); &__c[0];}))
+
 /******************************************************************/
 
 void RF24::csn(int mode) 
