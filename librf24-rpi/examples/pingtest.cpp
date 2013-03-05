3a14e793eeea589d332d557c5a560178123db367
Stanley Seow <stanleyseow@gmail.com>
2013-03-05 15:55:12 +0800

    Added rpi-hub-arduino sketch to examples folder for the arduino sketch matching for the rpi-hub examples

--- librf24-rpi/examples/pingtest.cpp
+++ librf24-rpi/examples/pingtest.cpp
@@ -32,7 +32,7 @@
 // Set up nRF24L01 radio on SPI bus plus pins 9 & 10
 
 //RF24 radio(9,10);
-RF24 radio("/dev/spidev0.0",8000000 , 25);  //spi device, speed and CSN,only CSN is NEEDED in RPI
+RF24 radio("/dev/spidev0.0",8000000 , 24);  //spi device, speed and CSN,only CSN is NEEDED in RPI
 
 
 // sets the role of this unit in hardware.  Connect to GND to be the 'pong' receiver
