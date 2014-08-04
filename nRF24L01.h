79ff183a5cd2236255bcadba273d85ab284787c6
Stanley Seow <stanleyseow@gmail.com>
2014-08-04 14:47:34 +0800

    Fixed Merge issues #2

--- nRF24L01.h
+++ nRF24L01.h
@@ -1,10 +1,6 @@
 /*
     Copyright (c) 2007 Stefan Engelke <mbox@stefanengelke.de>
-<<<<<<< HEAD
-    Portions Copyright (C) 2011 Greg Copeland
-=======
 	Portions Copyright (C) 2011 Greg Copeland
->>>>>>> 327f0609cf7b52b70accccc175b5809dd5074704
 
     Permission is hereby granted, free of charge, to any person
     obtaining a copy of this software and associated documentation
@@ -121,11 +117,7 @@
 #define LNA_HCURR   0
 
 /* P model memory Map */
-<<<<<<< HEAD
-#define RPD                  0x09
-=======
 #define RPD         0x09
->>>>>>> 327f0609cf7b52b70accccc175b5809dd5074704
 #define W_TX_PAYLOAD_NO_ACK  0xB0
 
 /* P model bit Mnemonics */
