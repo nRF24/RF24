90d85bef4bf370f54404e00805431f77a49607e7
TMRh20 <tmrh20@gmail.com>
2015-04-18 18:18:51 -0600

    Fix the rest of issue #101 per mz-fuzzy
    Per https://github.com/TMRh20/RF24/issues/101#issuecomment-94163382
    initialize pipe0_reading_address[0] to address "Weird and unstable
    behavior of python wrapper" identified by @kormitigrov   Resolution/Root
    Cause identified by mz-fuzzy

--- RF24.cpp
+++ RF24.cpp
@@ -413,6 +413,7 @@ RF24::RF24(uint8_t _cepin, uint8_t _cspin):
   ce_pin(_cepin), csn_pin(_cspin), p_variant(false),
   payload_size(32), dynamic_payloads_enabled(false), addr_width(5)//,pipe0_reading_address(0)
 {
+  pipe0_reading_address[0]=0;
 }
 
 /****************************************************************************/
@@ -420,7 +421,8 @@ RF24::RF24(uint8_t _cepin, uint8_t _cspin):
 #if defined (RF24_LINUX) && !defined (MRAA)//RPi constructor
 RF24::RF24(uint8_t _cepin, uint8_t _cspin, uint32_t _spi_speed):
   ce_pin(_cepin),csn_pin(_cspin),spi_speed(_spi_speed),p_variant(false), payload_size(32), dynamic_payloads_enabled(false),addr_width(5)//,pipe0_reading_address(0) 
-{	
+{
+  pipe0_reading_address[0]=0;
 }
 #endif
 
