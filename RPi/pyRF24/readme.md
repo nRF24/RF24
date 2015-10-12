c963ead59770d398d2e2b99407b203924904db04
doudz <sebastien.ramage@gmail.com>
2015-10-12 21:26:18 +0200

    python 3 support

--- RPi/pyRF24/readme.md
+++ RPi/pyRF24/readme.md
@@ -1,2 +1,10 @@
 Python Wrapper for RF24
 See http://tmrh20.github.io/RF24 for more information
+
+To compile for python 3
+python3 setup.py build
+sudo python3 setup.py install
+
+On Raspbian jessie, boost_python3 is missing
+you need to manually create the link
+ln -s /usr/lib/arm-linux-gnueabihf/libboost_python-py34.so.1.55.0 /usr/lib/arm-linux-gnueabihf/libboost_python3.so
--- RPi/pyRF24/setup.py
+++ RPi/pyRF24/setup.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
+import sys
+
+BOOST_LIB = 'boost_python'
+if sys.version_info >= (3,):
+    BOOST_LIB = 'boost_python3'
 
 module_RF24 = Extension('RF24',
-            libraries = ['rf24-bcm', 'boost_python'],
+            libraries = ['rf24-bcm', BOOST_LIB],
             sources = ['pyRF24.cpp'])
 
 setup(name='RF24',
