fdc9b4d249205979b167217f2d463b545c5b94be
ludiazv <ldiaz@atloideas.com>
2018-09-26 13:33:38 +0200

    SPIDEV GPIO exception and optimization(via caching sysfs) #407

--- utility/SPIDEV/gpio.h
+++ utility/SPIDEV/gpio.h
@@ -1,26 +1,34 @@
-/* 
+/*
  * https://github.com/mrshu/GPIOlib
  * Copyright (c) 2011, Copyright (c) 2011 mr.Shu
- * All rights reserved. 
- * 
+ * All rights reserved.
+ *
  * Modified on 24 June 2012, 11:06 AM
  * File:   gpio.h
  * Author: purinda (purinda@gmail.com)
- * 
+ *
  */
 
 #ifndef H
 #define	H
 
 #include <cstdio>
+#include <map>
+#include <stdexcept>
+
+/** Specific excpetion for SPI errors */
+class GPIOException : public std::runtime_error {
+	public:
+		explicit GPIOException(const std::string& msg) :  std::runtime_error(msg) { }
+};
 
 /**
  * @file gpio.h
  * \cond HIDDEN_SYMBOLS
  * Class declaration for GPIO helper files
  */
- 
- 
+
+
 /**
  * Example GPIO.h file
  *
@@ -29,7 +37,8 @@
  * See RF24_arch_config.h for additional information
  * @{
  */
- 
+
+typedef int GPIOfdCache_t;
 
 class GPIO {
 public:
@@ -37,12 +46,12 @@ public:
 	/* Constants */
 	static const int DIRECTION_OUT = 1;
 	static const int DIRECTION_IN = 0;
-	
+
 	static const int OUTPUT_HIGH = 1;
 	static const int OUTPUT_LOW = 0;
-		
+
 	GPIO();
-	
+
 	/**
 	 * Similar to Arduino pinMode(pin,mode);
      * @param port
@@ -50,7 +59,7 @@ public:
      */
 	static void open(int port, int DDR);
 	/**
-	 * 
+	 *
      * @param port
      */
 	static void close(int port);
@@ -64,17 +73,17 @@ public:
 	* Similar to Arduino digitalWrite(pin,state);
 	* @param port
 	* @param value
-	*/	
-	static void write(int port,int value);	
-	
-	virtual ~GPIO();
-	
-private:
+	*/
+	static void write(int port,int value);
 
+	virtual ~GPIO();
+
+private:
+  /* fd cache */
+  static std::map<int,GPIOfdCache_t> cache;
 };
 /**
  * \endcond
  */
 /*@}*/
 #endif	/* H */
-
