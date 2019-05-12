4220bf5ce6d804a7ea8a3056102636dda70203de
TMRh20 <tmrh20@gmail.com>
2019-05-12 17:18:25 -0600

    Fix broken stuff after using pgm_read_ptr

--- utility/LittleWire/RF24_arch_config.h
+++ utility/LittleWire/RF24_arch_config.h
@@ -17,6 +17,7 @@
   #define _BV(x) (1<<(x))
   #define pgm_read_word(p) (*(p))
   #define pgm_read_byte(p) (*(p))
+  #define pgm_read_ptr(p) (*(p))
   
   //typedef uint16_t prog_uint16_t;
   #define PSTR(x) (x)
--- utility/wiringPi/RF24_arch_config.h
+++ utility/wiringPi/RF24_arch_config.h
@@ -49,7 +49,7 @@ typedef uint16_t prog_uint16_t;
 #define pgm_read_word(p) (*(p))
 #define PRIPSTR "%s"
 #define pgm_read_byte(p) (*(p))
-
+#define pgm_read_ptr(p) (*(p))
 #endif // __ARCH_CONFIG_H__
 
 
