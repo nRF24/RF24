df3f8211976c4c457128f7a2dab606c2bea76728
Avamander <avamander@gmail.com>
2019-06-18 18:09:25 +0300

    Update RF24_config.h
    Updated copyright header

--- RF24_config.h
+++ RF24_config.h
@@ -1,14 +1,18 @@
 
 /*
- Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
-
+ Copyright (C) 
+  2011      J. Coliz <maniacbug@ymail.com>
+  2015-2019 TMRh20
+  2015      spaniakos <spaniakos@gmail.com>
+  2015      nerdralph
+  2015      zador-blood-stained
+  2016      akatran
+  2017-2019 Avamander <avamander@gmail.com>
+  2019      IkpeohaGodson
+         
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
  version 2 as published by the Free Software Foundation.
- */
- 
- /* spaniakos <spaniakos@gmail.com>
-  Added __ARDUINO_X86__ support
 */
 
 #ifndef __RF24_CONFIG_H__
