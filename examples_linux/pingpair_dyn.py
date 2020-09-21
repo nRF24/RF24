ff44f0f83470d3aee17fd55ee7d7d126a2f6a954
brendan <2bndy5@gmail.com>
2020-09-21 01:56:59 -0700

    name casing in pyRF24; pylinted pingpair & example

--- examples_linux/pingpair_dyn.py
+++ examples_linux/pingpair_dyn.py
@@ -2,9 +2,9 @@
 
 #
 # Example using Dynamic Payloads
-# 
+#
 #  This is an example of how to use payloads of a varying (dynamic) size.
-# 
+#
 
 from __future__ import print_function
 import time
@@ -21,10 +21,10 @@ irq_gpio_pin = None
 # ie: RF24 radio(<ce_pin>, <a>*10+<b>); spidev1.0 is 10, spidev1.1 is 11 etc..
 
 # Generic:
-radio = RF24(22,0);
+radio = RF24(22, 0)
 
 # RPi Alternate, with SPIDEV - Note: Edit RF24/arch/BBB/spi.cpp and  set 'this->device = "/dev/spidev0.0";;' or as listed in /dev
-#radio = RF24(22, 0);
+# radio = RF24(22, 0);
 
 
 # Setup for connected IRQ pin, GPIO 24 on RPi B+; uncomment to activate
@@ -36,13 +36,17 @@ def try_read_data(channel=0):
         while radio.available():
             len = radio.getDynamicPayloadSize()
             receive_payload = radio.read(len)
-            print('Got payload size={} value="{}"'.format(len, receive_payload.decode('utf-8')))
+            print(
+                'Got payload size={} value="{}"'.format(
+                    len, receive_payload.decode("utf-8")
+                )
+            )
             # First, stop listening so we can talk
             radio.stopListening()
 
             # Send the final one back.
             radio.write(receive_payload)
-            print('Sent response.')
+            print("Sent response.")
 
             # Now, resume listening so we catch the next packets.
             radio.startListening()
@@ -53,22 +57,26 @@ min_payload_size = 4
 max_payload_size = 32
 payload_size_increments_by = 1
 next_payload_size = min_payload_size
-inp_role = 'none'
-send_payload = b'ABCDEFGHIJKLMNOPQRSTUVWXYZ789012'
+inp_role = "none"
+send_payload = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ789012"
 millis = lambda: int(round(time.time() * 1000))
 
-print('pyRF24/examples/pingpair_dyn/')
+print("pyRF24/examples/pingpair_dyn/")
 radio.begin()
 radio.enableDynamicPayloads()
 radio.setRetries(5, 15)
 radio.printDetails()
 
-print(' ************ Role Setup *********** ')
-while (inp_role != '0') and (inp_role != '1'):
-    inp_role = str(input('Choose a role: Enter 0 for receiver, 1 for transmitter (CTRL+C to exit) '))
+print(" ************ Role Setup *********** ")
+while (inp_role != "0") and (inp_role != "1"):
+    inp_role = str(
+        input(
+            "Choose a role: Enter 0 for receiver, 1 for transmitter (CTRL+C to exit) "
+        )
+    )
 
-if inp_role == '0':
-    print('Role: Pong Back, awaiting transmission')
+if inp_role == "0":
+    print("Role: Pong Back, awaiting transmission")
     if irq_gpio_pin is not None:
         # set up callback for irq pin
         GPIO.setmode(GPIO.BCM)
@@ -79,20 +87,20 @@ if inp_role == '0':
     radio.openReadingPipe(1, pipes[0])
     radio.startListening()
 else:
-    print('Role: Ping Out, starting transmission')
+    print("Role: Ping Out, starting transmission")
     radio.openWritingPipe(pipes[0])
     radio.openReadingPipe(1, pipes[1])
 
 # forever loop
 while 1:
-    if inp_role == '1':  # ping out
+    if inp_role == "1":  # ping out
         # The payload will always be the same, what will change is how much of it we send.
 
         # First, stop listening so we can talk.
         radio.stopListening()
 
         # Take the time, and send it.  This will block until complete
-        print('Now sending length {} ... '.format(next_payload_size), end="")
+        print("Now sending length {} ... ".format(next_payload_size), end="")
         radio.write(send_payload[:next_payload_size])
 
         # Now, continue listening
@@ -107,14 +115,18 @@ while 1:
 
         # Describe the results
         if timeout:
-            print('failed, response timed out.')
+            print("failed, response timed out.")
         else:
             # Grab the response, compare, and send to debugging spew
             len = radio.getDynamicPayloadSize()
             receive_payload = radio.read(len)
 
             # Spew it
-            print('got response size={} value="{}"'.format(len, receive_payload.decode('utf-8')))
+            print(
+                'got response size={} value="{}"'.format(
+                    len, receive_payload.decode("utf-8")
+                )
+            )
 
         # Update size for next time.
         next_payload_size += payload_size_increments_by
--- pyRF24/setup.py
+++ pyRF24/setup.py
@@ -5,18 +5,21 @@ import sys
 import setuptools
 import crossunixccompiler
 
-version = ''
+version = ""
 
 
 def process_configparams():
     global version
-
-    with open('../Makefile.inc') as f:
+    # can't access "../Makefile.inc" from working dir because it's relative.
+    # brute force absolute path dynamically.
+    script_dir = os.path.split(os.path.abspath(__file__))[0]
+    abs_file_path = os.path.join(script_dir, "Makefile.inc")
+    with open(abs_file_path) as f:
         config_lines = f.read().splitlines()
 
     cflags = os.getenv("CFLAGS", "")
     for line in config_lines:
-        identifier, value = line.split('=', 1)
+        identifier, value = line.split("=", 1)
         if identifier == "CPUFLAGS":
             cflags += " " + value
         elif identifier == "HEADER_DIR":
@@ -32,17 +35,15 @@ def process_configparams():
 
 
 if sys.version_info >= (3,):
-    BOOST_LIB = 'boost_python3'
+    BOOST_LIB = "boost_python3"
 else:
-    BOOST_LIB = 'boost_python'
+    BOOST_LIB = "boost_python"
 
 process_configparams()
 crossunixccompiler.register()
 
-module_RF24 = setuptools.Extension('RF24',
-                                   libraries=['rf24', BOOST_LIB],
-                                   sources=['pyRF24.cpp'])
+module_RF24 = setuptools.Extension(
+    "python-nrf24l01", libraries=["rf24", BOOST_LIB], sources=["pyRF24.cpp"]
+)
 
-setuptools.setup(name='RF24',
-                 version=version,
-                 ext_modules=[module_RF24])
+setuptools.setup(name="python-nrf24l01", version=version, ext_modules=[module_RF24])
