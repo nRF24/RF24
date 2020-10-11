77f22cbd64a84ea8e1cf7cab23b9cdc26e59e204
Stephen Crane <jscrane@gmail.com>
2020-10-11 11:24:03 +0100

    fix formatting

--- utility/ATTiny/RF24_arch_config.h
+++ utility/ATTiny/RF24_arch_config.h
@@ -7,40 +7,38 @@
 #ifndef __RF24_ARCH_CONFIG_H__
 #define __RF24_ARCH_CONFIG_H__
 
-  /*** USER DEFINES:  ***/  
-  //#define FAILURE_HANDLING
-  //#define MINIMAL
-  /**********************/
+/*** USER DEFINES:  ***/
+//#define FAILURE_HANDLING
+//#define MINIMAL
+/**********************/
 
-  #define rf24_max(a,b) (a>b?a:b)
-  #define rf24_min(a,b) (a<b?a:b)
+#define rf24_max(a, b) (a>b?a:b)
+#define rf24_min(a, b) (a<b?a:b)
 
+#if ARDUINO < 100
+    #include <WProgram.h>
+#else
+    #include <Arduino.h>
+#endif
 
+#include <stddef.h>  
 
-  #if ARDUINO < 100
-	#include <WProgram.h>
-  #else
-	#include <Arduino.h>
-  #endif
-  #include <stddef.h>  
-  
-  #include <SPI.h>
+#include <SPI.h>
 
-  #define _SPI SPI
-  
-  #ifdef SERIAL_DEBUG
-	#define IF_SERIAL_DEBUG(x) ({x;})
-  #else
-	#define IF_SERIAL_DEBUG(x)
-	#if defined(RF24_TINY)
-	#define printf_P(...)
+#define _SPI SPI
+
+#ifdef SERIAL_DEBUG
+    #define IF_SERIAL_DEBUG(x) ({x;})
+#else
+    #define IF_SERIAL_DEBUG(x)
+    #if defined(RF24_TINY)
+        #define printf_P(...)
     #endif
-  #endif
+#endif
 
-  #include <avr/pgmspace.h>
-  #define PRIPSTR "%S"
-  
-  
+#include <avr/pgmspace.h>
+
+#define PRIPSTR "%S"
 
 #endif // __RF24_ARCH_CONFIG_H__
 
