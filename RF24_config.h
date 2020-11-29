8b2f2a952d7ac9c389be6d5a39a0ca0178a3234a
Stephen Crane <jscrane@gmail.com>
2020-11-29 12:15:30 +0000

    Make RF24_POWERUP_DELAY configurable in RF24_config.h (#690)
    * available() could be slightly faster #631
    
    * allow skipping delay(5) in powerUp()
    
    * allow skipping delay(5) in powerUp()
    
    * Revert "allow skipping delay(5) in powerUp()"
    
    This reverts commit ffd7524a8c787507f820c7c5384874e232cdb4f4.
    
    * Revert "allow skipping delay(5) in powerUp()"
    
    This reverts commit ffd7524a8c787507f820c7c5384874e232cdb4f4.
    
    * add RF24_POWERUP_DELAY_MS to RF24_config.h
    
    * delayMicroseconds()
    
    * update doc comment

--- RF24_config.h
+++ RF24_config.h
@@ -18,13 +18,22 @@
 #ifndef __RF24_CONFIG_H__
 #define __RF24_CONFIG_H__
 
-/*** USER DEFINES:    ***/    
+/*** USER DEFINES:    ***/
 #define FAILURE_HANDLING
 //#define SERIAL_DEBUG
 //#define MINIMAL
 //#define SPI_UART    // Requires library from https://github.com/TMRh20/Sketches/tree/master/SPI_UART
 //#define SOFTSPI     // Requires library from https://github.com/greiman/DigitalIO
-    
+ 
+/**
+ * User access to internally used delay time (in microseconds) during RF24::powerUp()
+ * @warning This default value compensates for all supported hardware. Only adjust this if you
+ * know your radio's hardware is, in fact, genuine and reliable.
+ */
+#if !defined(RF24_POWERUP_DELAY)
+#define RF24_POWERUP_DELAY	5000
+#endif
+
 /**********************/
 #define rf24_max(a,b) (a>b?a:b)
 #define rf24_min(a,b) (a<b?a:b)
