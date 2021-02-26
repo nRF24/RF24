33541ed1801ee98c2d6a3757a9f6f3715d157d9f
Brendan <2bndy5@gmail.com>
2021-02-25 18:11:12 -0800

    ammend comment

--- RF24.cpp
+++ RF24.cpp
@@ -754,7 +754,7 @@ bool RF24::begin(void)
     write_register(DYNPD, 0);         // disable dynamic payloads by default (for all pipes)
     dynamic_payloads_enabled = false;
     write_register(EN_AA, 0x3F);      // enable auto-ack on all pipes
-    write_register(EN_RXADDR, 3);     // close all RX pipes
+    write_register(EN_RXADDR, 3);     // only open RX pipes 0 & 1
     setPayloadSize(32);               // set static payload size to 32 (max) bytes by default
     setAddressWidth(5);               // set default address length to (max) 5 bytes
 
