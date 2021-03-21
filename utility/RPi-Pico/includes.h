9847c1ad9fa2491461404113ea7ab067be3c3ac5
Jannis Achstetter <kripton@kripserver.net>
2021-03-21 23:25:48 +0100

    Copy the utility template and give it a name

---
+++ utility/RPi-Pico/includes.h
@@ -0,0 +1,29 @@
+/**
+ * @file includes.h
+ * Configuration defines for RF24/Linux
+ */
+
+/**
+ * Example of includes.h for RF24 Linux portability
+ *
+ * @defgroup Porting_Includes Porting: Includes
+ *
+ * @{
+ */
+
+#ifndef __RF24_INCLUDES_H__
+#define __RF24_INCLUDES_H__
+
+/**
+ * Define a specific platform for this configuration
+ */
+#define RF24_RPI_PICO
+
+/**
+ * Load the correct configuration for this platform
+ */
+#include "RPi-Pico/RF24_arch_config.h"
+
+#endif
+
+/**@}*/
