844394e720b3a2c784fb242607cc1a3daef95fcd
Brendan <2bndy5@gmail.com>
2021-04-13 00:54:02 -0700

    revert change to txStandby()

--- RF24.cpp
+++ RF24.cpp
@@ -1297,10 +1297,9 @@ bool RF24::txStandBy(uint32_t timeout, bool startTx)
 
     while (!(read_register(FIFO_STATUS) & _BV(TX_EMPTY))) {
         if (status & _BV(MAX_RT)) {
-            // transmission failure detected
-            ce(LOW);                                 // exit active TX mode
-            write_register(NRF_STATUS, _BV(MAX_RT)); // clear data_fail flag
-            ce(HIGH);                                // re-enter active TX mode
+            write_register(NRF_STATUS, _BV(MAX_RT));
+            ce(LOW); // Set re-transmit
+            ce(HIGH);
             if (millis() - start >= timeout) {
                 ce(LOW);
                 flush_tx();
