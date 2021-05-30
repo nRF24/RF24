a31d3e3dd10561592e7b75d669cabb6f68112bf8
Brendan <2bndy5@gmail.com>
2021-05-30 02:38:07 -0700

    attempt to patch setup.py for use with CMake

--- pyRF24/setup.py
+++ pyRF24/setup.py
@@ -10,6 +10,7 @@ version = ""
 
 def process_configparams():
     version = ""  # using python keyword `global` is bad practice
+    cflags = os.getenv("CFLAGS", "")
 
     # NOTE current repo directory structure requires the use of
     # `python3 setup.py build` and `python3 setup.py install`
@@ -17,27 +18,62 @@ def process_configparams():
     # `tmp` folder that doesn't have the needed `../Makefile.inc`
     # NOTE can't access "../Makefile.inc" from working dir because
     # it's relative. Brute force absolute path dynamically.
-    script_dir = os.path.split(os.path.abspath(os.getcwd()))[0]
-    abs_file_path = os.path.join(script_dir, "Makefile.inc")
-    with open(abs_file_path) as f:
-        config_lines = f.read().splitlines()
+    rf24_git_dir = os.path.split(os.path.abspath(os.getcwd()))[0]
+    try:  # get compiler options from the generated Makefile.inc
+        symlink_directory = ""
+        with open(os.path.join(rf24_git_dir, "Makefile.inc"), "r") as f:
+            for line in f.read().splitlines():
+                identifier, value = line.split('=', 1)
+                if identifier == "CPUFLAGS":
+                    # this is never reached as CPUFLAGS var is concatenated into CFLAGS var.
+                    # however the CFLAGS var may conditionally contain "-lwiringPi"
+                    cflags += " " + value
+                elif identifier == "HEADER_DIR":
+                    cflags += " -I" + os.path.dirname(value)
+                elif identifier == "LIB_DIR":
+                    symlink_directory = value
+                    cflags += " -L" + value
+                elif identifier == "LIB_VERSION":
+                    version = value
+                elif identifier in ("CC", "CXX"):
+                    os.environ[identifier] = value
 
-    cflags = os.getenv("CFLAGS", "")
-    for line in config_lines:
-        identifier, value = line.split('=', 1)
-        if identifier == "CPUFLAGS":
-            cflags += " " + value
-        elif identifier == "HEADER_DIR":
-            cflags += " -I" + os.path.dirname(value)
-        elif identifier == "LIB_DIR":
-            cflags += " -L" + value
-        elif identifier == "LIB_VERSION":
-            version = value
-        elif identifier in ("CC", "CXX"):
-            os.environ[identifier] = value
+        # check C++ RF24 lib is installed
+        try:
+            with open(symlink_directory + "/librf24.so", "r+b") as f:
+                pass
+        except FileNotFoundError:
+            raise FileNotFoundError("RF24 library is not installed. "
+                                    "{}}/librf24.so does not exist.".format(
+                                        symlink_directory
+                                    ))
+
+    except FileNotFoundError:  # assuming lib was built & installed with CMake
+        # the following algorithm does not work for cross-compilation.
+
+        # verify the C++ RF24 lib is installed
+        try:
+            with open("/usr/local/lib/librf24.so", "r+b"):
+                pass
+        except FileNotFoundError:
+            raise FileNotFoundError("RF24 library is not installed. "
+                                    "/usr/local/lib/librf24.so does not exist.")
+
+        # get LIB_VERSION from library.properties file for Arduino IDE
+        with open(os.path.join(rf24_git_dir, "library.properties"), "r") as f:
+            for line in f.read().splitlines():
+                if line.startswith("version"):
+                    version = line.split("=")[1]
+        # add default values for compilation
+        # cflags += "-Ofast -Wall -pthread"  # default flags agnostic to GNU compiler
+        cflags += " -I/usr/local/include/RF24"  # default HEADER_DIR
+        cflags += " -L/usr/local/lib"  # default LIB_DIR
+        os.environ["CC"] = "/usr/bin/cc"  # default C compiler
+        os.environ["CXX"] = "/usr/bin/c++"  # default C++ compiler
 
     os.environ["CFLAGS"] = cflags
     return version
+
 
 if sys.version_info >= (3,):
     BOOST_LIB = "boost_python3"
