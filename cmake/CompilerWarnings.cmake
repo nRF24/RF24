002fea0955606e3254a98de40190bde8f60ce2f5
2bndy5 <2bndy5@gmail.com>
2021-08-23 16:03:18 -0700

    revert SPI feat 4 Teensy; disable -Wold-style-cast

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -3,7 +3,7 @@
 # https://github.com/lefticus/cppbestpractices/blob/master/02-Use_the_Tools_Available.md
 
 function(set_project_warnings project_name)
-    option(WARNINGS_AS_ERRORS "Treat compiler warnings as errors" FALSE)
+    option(WARNINGS_AS_ERRORS "Treat compiler warnings as errors" TRUE)
 
     set(MSVC_WARNINGS
         /W4 # Baseline reasonable warnings
@@ -37,7 +37,7 @@ function(set_project_warnings project_name)
         -Wshadow # warn the user if a variable declaration shadows one from a parent context
         -Wnon-virtual-dtor # warn the user if a class with virtual functions has a non-virtual destructor. This helps
                            # catch hard to track down memory errors
-        -Wold-style-cast # warn for c-style casts
+        # -Wold-style-cast # warn for c-style casts
         -Wcast-align # warn for potential performance problem casts
         -Wunused # warn on anything being unused
         -Woverloaded-virtual # warn if you overload (not override) a virtual function
