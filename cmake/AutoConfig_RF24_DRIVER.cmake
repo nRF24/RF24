a89b967ddae556e994e3725c4085446fd67df133
2bndy5 <2bndy5@gmail.com>
2022-01-09 14:29:47 -0800

    update build systems (needs HW testing)

--- cmake/AutoConfig_RF24_DRIVER.cmake
+++ cmake/AutoConfig_RF24_DRIVER.cmake
@@ -5,7 +5,8 @@ set(RF24_DRIVER "UNKNOWN" CACHE STRING "override automatic configuration of RF24
     RPi
     SPIDEV
     MRAA
-    LittleWire"
+    LittleWire
+    pigpio"
 )
 
 ###########################
@@ -28,15 +29,18 @@ endif()
 if(${RF24_DRIVER} STREQUAL "UNKNOWN") # invokes automatic configuration
     if("${SOC}" STREQUAL "BCM2708" OR "${SOC}" STREQUAL "BCM2709" OR "${SOC}" STREQUAL "BCM2835")
         set(RF24_DRIVER RPi CACHE STRING "using folder /utility/RPi" FORCE)
-    elseif(NOT "${LibMRAA}" STREQUAL "LibMRAA-NOTFOUND")
-        message(STATUS "Found MRAA library: ${LibMRAA}")
-        set(RF24_DRIVER MRAA CACHE STRING "using folder /utility/MRAA" FORCE)
+    elseif(NOT "${PIGPIO}" STREQUAL "PIGPIO-NOTFOUND")
+        message(STATUS "Found MRAA library: ${PIGPIO}")
+        set(RF24_DRIVER PIGPIO CACHE STRING "using folder /utility/pigpio" FORCE)
     elseif(NOT "${LibWiringPi}" STREQUAL "LibWiringPi-NOTFOUND")
         message(STATUS "Found wiringPi library: ${LibWiringPi}")
         set(RF24_DRIVER wiringPi CACHE STRING "using folder /utility/wiringPi" FORCE)
     elseif(NOT "${LibLittleWire}" STREQUAL "LibLittleWire-NOTFOUND")
         message(STATUS "Found LittleWire library: ${LibLittleWire}")
         set(RF24_DRIVER LittleWire CACHE STRING "using folder /utility/LittleWire" FORCE)
+    elseif(NOT "${LibMRAA}" STREQUAL "LibMRAA-NOTFOUND")
+        message(STATUS "Found MRAA library: ${LibMRAA}")
+        set(RF24_DRIVER MRAA CACHE STRING "using folder /utility/MRAA" FORCE)
     elseif(SPIDEV_EXISTS) # should be a non-empty string if SPI is enabled
         message(STAUS "detected that SPIDEV is enabled: ${SPIDEV_EXISTS}")
         set(RF24_DRIVER SPIDEV CACHE STRING "using folder /utility/SPIDEV" FORCE)
