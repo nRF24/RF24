c1527a6dc554e59990e3c2bc2bc545146ba748d4
2bndy5 <2bndy5@gmail.com>
2022-01-27 23:18:58 -0800

    revert comment change in copyright notices

--- RF24.h
+++ RF24.h
@@ -1,5 +1,6 @@
 /*
  Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
+
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
  version 2 as published by the Free Software Foundation.
