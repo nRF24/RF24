0b16d27f87a9e3dcb9837433277c28a0c0cf4548
Brendan <2bndy5@gmail.com>
2022-06-20 05:08:29 -0700

    fix scanner.py

--- examples_linux/scanner.py
+++ examples_linux/scanner.py
@@ -9,6 +9,7 @@ try:
     from rich.progress import BarColumn, Progress, TextColumn
     from rich.live import Live
     from rich.prompt import Prompt, IntPrompt
+    from rich.style import Style
 except ImportError as exc:
     raise ImportError(
         "This example requires the python `rich` module installed."
@@ -34,14 +35,17 @@ for i, rate in enumerate(offered_rates):
 table = Table.grid(padding=(0, 1))
 totals = [0] * 126  # for the total signal count on each channel
 signals = [0] * 126  # for tracking the signal count on each channel
-CACHED_MAX = 5  # the depth of history to calculate averages with
-history = [[0] * CACHED_MAX] * 126  # for tracking peak decay on each channel
-progress_bars = [Progress()] * 126
+CACHE_MAX = 5  # the depth of history to calculate averages with
+history = [[0] * CACHE_MAX] * 126  # for tracking peak decay on each channel
+progress_bars = [None] * 126
 for i in range(21):  # 21 rows
     row = []
     for j in range(i, i + (21 * 6), 21):  # 6 columns
+        BG_COLOR = "#8787d7" if (j % 21) % 2 else "#5f5f5f"
         progress_bars[j] = Progress(
-            TextColumn("{task.description}"), BarColumn(), TextColumn("{task.signals}")
+            TextColumn("{task.description}", style=Style(bgcolor=BG_COLOR)),
+            BarColumn(style=Style(bgcolor=BG_COLOR)),
+            TextColumn("{task.fields[signals]}", style=Style(bgcolor=BG_COLOR)),
         )
         progress_bars[j].add_task(f"{2400 + (j)}", signals=0)  # only 1 task for each
         row.append(progress_bars[j])
@@ -54,10 +58,6 @@ radio.setDataRate(available_rates[DATA_RATE])
 
 DURATION = IntPrompt.ask("Enter the scan duration (in whole seconds)")
 SELECTED_RATE = offered_rates[DATA_RATE]
-console.print(
-    f"Beginning scan using {SELECTED_RATE} for {DURATION} seconds.",
-    "Channel labels are in MHz.",
-)
 
 
 def scan_channel(channel: int) -> int:
@@ -75,36 +75,47 @@ def calc_peak(channel: int) -> int:
     total = int(signals[channel])
     for val in history[channel]:
         total += val
-    return math.ceil(total / (CACHED_MAX + 1))
+    return math.ceil(total / (CACHE_MAX + 1))
 
 
-# perform scan
-TIMEOUT = time.monotonic() + DURATION
-with Live(table, refresh_per_second=1000) as live:
-    try:
-        history_index: int = 0
-        while time.monotonic() < TIMEOUT:
-            for chl, bar in enumerate(progress_bars):
-                # save the latest in history
-                history[chl][history_index] = signals[chl]
+def scan():
+    """Perform scan."""
+    timeout = time.monotonic() + DURATION
+    console.print(
+        f"Beginning scan using {SELECTED_RATE} for {DURATION} seconds.",
+        "Channel labels are in MHz.",
+    )
+    with Live(table, refresh_per_second=1000):
+        try:
+            history_index: int = 0
+            while time.monotonic() < timeout:
+                for chl, p_bar in enumerate(progress_bars):
+                    # save the latest in history
+                    history[chl][history_index] = signals[chl]
 
-                # refresh the latest
-                signals[chl] = scan_channel(chl)
+                    # refresh the latest
+                    signals[chl] = scan_channel(chl)
 
-                # update total signal count for the channel
-                totals[chl] += signals[chl]
+                    # update total signal count for the channel
+                    totals[chl] += signals[chl]
 
-                peak = calc_peak(chl)  # average latest with history
-                bar.update(
-                    bar.task_ids[0],
-                    completed=signals[chl],
-                    total=CACHED_MAX + 1,
-                    signals=totals[chl],
+                    peak = calc_peak(chl)  # average latest with history
+                    p_bar.update(
+                        p_bar.task_ids[0],
+                        completed=peak,
+                        total=CACHE_MAX + 1,
+                        signals=totals[chl],
+                    )
+                # history is a list of cyclical arrays. Wrap iterator to 0 at CACHE_MAX
+                history_index = (
+                    0 if history_index == (CACHE_MAX - 1) else history_index + 1
                 )
-            # history is a list of cyclical arrays. Wrap iterator to 0 at CAHCED_MAX
-            history_index = (
-                0 if history_index == (CACHED_MAX - 1) else history_index + 1
-            )
-    except KeyboardInterrupt:
-        console.print(" Keyboard interrupt detected. Powering down radio.")
-        radio.powerDown()
+        except KeyboardInterrupt:
+            console.print(" Keyboard interrupt detected. Powering down radio.")
+            radio.powerDown()
+
+
+if __name__ == "__main__":
+    scan()
+else:
+    console.print("Enter `scan()` to run a scan.")
