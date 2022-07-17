690b4ed98c833c40324862d4749b4fa48ede90b7
Brendan <2bndy5@gmail.com>
2022-07-17 10:00:16 -0700

    ran clang-format

--- examples_pico/scanner.cpp
+++ examples_pico/scanner.cpp
@@ -58,7 +58,7 @@ uint8_t values[num_channels];     // the array to store summary of signal counts
 // To detect noise, we'll use the worst addresses possible (a reverse engineering tactic).
 // These addresses are designed to confuse the radio into thinking
 // that the RF signal's preamble is part of the packet/payload.
-const uint8_t noiseAddress[][6] = {{0x55, 0x55}, {0xAA, 0xAA}, {0x0A, 0xAA}, {0xA0, 0xAA}, {0x00,0xAA}, {0xAB,0xAA}};
+const uint8_t noiseAddress[][6] = {{0x55, 0x55}, {0xAA, 0xAA}, {0x0A, 0xAA}, {0xA0, 0xAA}, {0x00, 0xAA}, {0xAB, 0xAA}};
 
 const int num_reps = 100; // number of passes for each scan of the entire spectrum
 
