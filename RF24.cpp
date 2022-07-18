3f786bffd5e27bd67bbe518bcc3249885fde05ec
Brendan <2bndy5@gmail.com>
2022-07-18 06:42:46 -0700

    remove trailing whitespace and add a blank line

--- RF24.cpp
+++ RF24.cpp
@@ -342,6 +342,7 @@ void RF24::write_payload(const void* buf, uint8_t data_len, const uint8_t writeT
 
     *ptx++ = writeType;
     while (data_len--) *ptx++ = *current++;
+
     while (blank_len--) *ptx++ = 0;
 
     #if defined(RF24_RP2)
@@ -359,7 +360,7 @@ void RF24::write_payload(const void* buf, uint8_t data_len, const uint8_t writeT
     #if defined(RF24_SPI_PTR)
     status = _spi->transfer(writeType);
     while (data_len--) _spi->transfer(*current++);
-    
+
     while (blank_len--) _spi->transfer(0);
 
     #else // !defined(RF24_SPI_PTR)
@@ -435,7 +436,7 @@ void RF24::read_payload(void* buf, uint8_t data_len)
     #else // !defined(RF24_SPI_PTR)
     status = _SPI.transfer(R_RX_PAYLOAD);
     while (data_len--) *current++ = _SPI.transfer(0xFF);
-    
+
     while (blank_len--) _SPI.transfer(0xff);
 
     #endif // !defined(RF24_SPI_PTR)
