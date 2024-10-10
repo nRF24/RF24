6cc8a252c2882012843a13414a30f3e3c16d75bd
LennartF22 <18723691+LennartF22@users.noreply.github.com>
2024-10-10 16:56:47 +0200

    Use internal bus configuration in example

--- examples_esp/getting_started/src/main.cpp
+++ examples_esp/getting_started/src/main.cpp
@@ -21,38 +21,10 @@ RF24 radio = RF24(CE_PIN, CSN_PIN);
 
 static const char* TAG = "RF24/examples_esp/getting_started";
 
-spi_bus_config_t busConfig;
-
 extern "C" void app_main(void)
 {
-#ifdef RF24_DEFAULT_MOSI
-    printf("using MOSI pin %d!\n", RF24_DEFAULT_MOSI);
-    busConfig.mosi_io_num = RF24_DEFAULT_MOSI;
-#else
-    busConfig.mosi_io_num = -1; // GPIO13 on SPI2_HOST; GPIO23 on SPI3_HOST
-#endif
-#ifdef RF24_DEFAULT_MISO
-    printf("using MISO pin %d!\n", RF24_DEFAULT_MISO);
-    busConfig.miso_io_num = RF24_DEFAULT_MISO;
-#else
-    busConfig.miso_io_num = -1; // GPIO12 on SPI2_HOST; GPIO19 on SPI3_HOST
-#endif
-#ifdef RF24_DEFAULT_SCLK
-    printf("using SCLK pin %d!\n", RF24_DEFAULT_SCLK);
-    busConfig.sclk_io_num = RF24_DEFAULT_SCLK;
-#else
-    busConfig.sclk_io_num = -1; // GPIO14 on SPI2_HOST; GPIO18 on SPI3_HOST
-#endif
-    busConfig.quadwp_io_num = -1;
-    busConfig.quadhd_io_num = -1;
-    busConfig.data4_io_num = -1;
-    busConfig.data5_io_num = -1;
-    busConfig.data6_io_num = -1;
-    busConfig.data7_io_num = -1;
-    busConfig.max_transfer_sz = 33; // RF24 lib only buffers 33 bytes for SPI transactions
-
     SPIClass spi;
-    spi.begin(SPI2_HOST, RF24_SPI_SPEED, SPI_MODE0, &busConfig);
+    spi.begin(SPI2_HOST);
     printf("SPI bus configured\n");
     printf("using CE_PIN pin %d!\n", CE_PIN);
     printf("using CSN_pin pin %d!\n", CSN_PIN);
