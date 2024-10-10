b354b86c892525f4d49a2a91f6b206bad0e3fe4f
Brendan <2bndy5@gmail.com>
2024-10-09 18:54:14 -0700

    more WIP changes

--- utility/esp_idf/gpio.cpp
+++ utility/esp_idf/gpio.cpp
@@ -6,13 +6,18 @@ GPIO::GPIO()
 
 void GPIO::open(rf24_gpio_pin_t port, gpio_mode_t direction)
 {
-    gpio_config_t pin_conf;
-    pin_conf.pin_bit_mask = (1 << port);
-    pin_conf.mode = direction;
-    pin_conf.pull_up_en = GPIO_PULLUP_DISABLE;
-    pin_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    pin_conf.intr_type = GPIO_INTR_DISABLE;
-    esp_err_t ret = gpio_config(&pin_conf);
+    // gpio_config_t pin_conf;
+    // pin_conf.pin_bit_mask = (1 << port);
+    // pin_conf.mode = direction;
+    // pin_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    // pin_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    // pin_conf.intr_type = GPIO_INTR_DISABLE;
+    // esp_err_t ret = gpio_config(&pin_conf);
+    // ESP_ERROR_CHECK(ret);
+    close(port);
+    esp_err_t ret = gpio_set_direction((gpio_num_t)port, direction);
+    ESP_ERROR_CHECK(ret);
+    ret = gpio_set_level((gpio_num_t)port, (uint32_t)0);
     ESP_ERROR_CHECK(ret);
 }
 
