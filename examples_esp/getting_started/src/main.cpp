4e205cee61c88dd970f80158e78c3badb0c67b69
Lennart <18723691+LennartF22@users.noreply.github.com>
2024-10-11 01:51:01 +0200

    Fix ESP-IDF support (#1005)
    * Remove TinyUSB code
    * Use USB CDC for ESP32-S2 and USB Serial JTAG for ESP32-S3
    * Add support for adding local PlatformIO overrides
    * Fix SPI pin initialization
      And stop setting properties that are in a "union" with the actual pin numbers.
    * Fix SPI mode
    * Properly initialize device config struct
    * Remove not properly working status register check
      The check did not work properly, since CS is not asserted!
    * Make sure transaction functions are called
      It also worked without it, but if we don't acquire the bus, we will have
      issues when the bus is used by multiple devices and we control the CS line
      manually.
    * Stop using DMA for small transactions
      DMA usually performs worse than no DMA for transactions with just a few dozen
      of bytes, thus we disable it.
    * Reduce queue_size to 1
      No benefit in using a larger queue_size, since we are using blocking/polling
      transactions.
    * Use max_transfer_sz again
    * Remove unnecessary properties
    * Use `extern "C"` for app_main function only
    * Use internal bus configuration in example

--- examples_esp/getting_started/src/main.cpp
+++ examples_esp/getting_started/src/main.cpp
@@ -2,15 +2,8 @@
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "tinyusb.h"
-#include "tusb_cdc_acm.h"
-#include "tusb_console.h"
 #include "sdkconfig.h"
 #include <driver/spi_master.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 #ifdef RF24_CE_PIN
     #define CE_PIN RF24_CE_PIN
@@ -28,56 +21,13 @@ RF24 radio = RF24(CE_PIN, CSN_PIN);
 
 static const char* TAG = "RF24/examples_esp/getting_started";
 
-spi_bus_config_t busConfig;
-
-void setupSerialUSB(); // See definition below
-
-void app_main(void)
+extern "C" void app_main(void)
 {
-    setupSerialUSB();
-
-#ifdef RF24_DEFAULT_MOSI
-    printf("using MOSI pin %d!\n", RF24_DEFAULT_MOSI);
-    busConfig.mosi_io_num = RF24_DEFAULT_MOSI;
-#else
-    busConfig.mosi_io_num = -1; // GPIO13 on SPI2_HOST; GPIO23 on SPI3_HOST
-#endif
-    busConfig.data0_io_num = -1;
-#ifdef RF24_DEFAULT_MISO
-    printf("using MISO pin %d!\n", RF24_DEFAULT_MISO);
-    busConfig.miso_io_num = RF24_DEFAULT_MISO;
-#else
-    busConfig.miso_io_num = -1; // GPIO12 on SPI2_HOST; GPIO19 on SPI3_HOST
-#endif
-    busConfig.data1_io_num = -1;
-#ifdef RF24_DEFAULT_SCLK
-    printf("using SCLK pin %d!\n", RF24_DEFAULT_SCLK);
-    busConfig.sclk_io_num = RF24_DEFAULT_SCLK;
-#else
-    busConfig.sclk_io_num = -1; // GPIO14 on SPI2_HOST; GPIO18 on SPI3_HOST
-#endif
-    busConfig.quadwp_io_num = -1;
-    // busConfig.data2_io_num = -1;
-    busConfig.quadhd_io_num = -1;
-    // busConfig.data3_io_num = -1;
-    // busConfig.data4_io_num = -1;
-    // busConfig.data5_io_num = -1;
-    // busConfig.data6_io_num = -1;
-    // busConfig.data7_io_num = -1;
-    // busConfig.max_transfer_sz = 33; // RF24 lib only buffers 33 bytes for SPI transactions
-    // busConfig.flags = 0;
-    // busConfig.isr_cpu_id = ESP_INTR_CPU_AFFINITY_AUTO;
-    // busConfig.intr_flags = 0;
-
     SPIClass spi;
-    spi.begin(SPI2_HOST, /* RF24_SPI_SPEED */ 4000000, SPI_MODE1, &busConfig);
+    spi.begin(SPI2_HOST);
     printf("SPI bus configured\n");
     printf("using CE_PIN pin %d!\n", CE_PIN);
     printf("using CSN_pin pin %d!\n", CSN_PIN);
-    spi.beginTransaction();
-    uint8_t config_reg = spi.transfer(0);
-    spi.endTransaction();
-    printf("CONFIG register (before): %02x\n", config_reg);
 
     bool ok = radio.begin(&spi);
     printf("finished attempt to init radio\n");
@@ -87,55 +37,7 @@ void app_main(void)
         printf("Success!! radio is ready to configure.\n");
     }
 
-    spi.beginTransaction();
-    config_reg = spi.transfer(0);
-    spi.endTransaction();
-    printf("CONFIG register (after): %02x\n", config_reg);
-
     while (true) {
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
-
-/** Setup TinyUSB for CDC serial communication */
-void setupSerialUSB()
-{
-    ESP_LOGI(TAG, "USB initialization");
-    const tinyusb_config_t tusb_cfg = {
-        .device_descriptor = NULL, // Use the default device descriptor specified in Menuconfig
-        .string_descriptor = NULL, // Use the default string descriptors specified in Menuconfig
-        .string_descriptor_count = 0,
-        .external_phy = false, // Use internal USB PHY (in most cases)
-#if (TUD_OPT_HIGH_SPEED)
-        .fs_configuration_descriptor = NULL, // Use the default full-speed configuration descriptor according to settings in Menuconfig
-        .hs_configuration_descriptor = NULL, // Use the default high-speed configuration descriptor according to settings in Menuconfig
-        .qualifier_descriptor = NULL,        // Use the default qualifier descriptor, with values from default device descriptor
-#else
-        .configuration_descriptor = NULL, // Use the default configuration descriptor according to settings in Menuconfig
-#endif // TUD_OPT_HIGH_SPEED
-        .self_powered = false,
-        .vbus_monitor_io = 0,
-    };
-    ESP_ERROR_CHECK(tinyusb_driver_install(&tusb_cfg));
-
-    const tinyusb_config_cdcacm_t acm_cfg = {
-        .usb_dev = TINYUSB_USBDEV_0,
-        .cdc_port = TINYUSB_CDC_ACM_0,
-        .rx_unread_buf_sz = 64,
-        .callback_rx = NULL,
-        .callback_rx_wanted_char = NULL,
-        .callback_line_state_changed = NULL,
-        .callback_line_coding_changed = NULL};
-    ESP_ERROR_CHECK(tusb_cdc_acm_init(&acm_cfg));
-
-    ESP_LOGI(TAG, "USB initialization DONE");
-    esp_tusb_init_console(TINYUSB_CDC_ACM_0); // log to usb
-
-    // the delay here isn't needed, but any `abort()` called immediately after this function would
-    // prevent the OS from seeing the USB port enumerated as a CDC Serial device
-    vTaskDelay(1000 / portTICK_PERIOD_MS); // remove this when lib is working properly
-}
-
-#ifdef __cplusplus
-}
-#endif
--- utility/esp_idf/spi.cpp
+++ utility/esp_idf/spi.cpp
@@ -8,6 +8,7 @@ SPIClass::SPIClass() : bus(nullptr)
 void SPIClass::begin(spi_host_device_t busNo, uint32_t speed)
 {
     spi_bus_config_t busConfig;
+    memset(&busConfig, 0, sizeof(busConfig));
     // We will use the pins corresponding to the specified busNo (appropriate config option).
     // Users can also pass a customized bus config to SPIClass::begin() overload if/when
     // a secondary SPI bus is desired.
@@ -18,7 +19,6 @@ void SPIClass::begin(spi_host_device_t busNo, uint32_t speed)
 #else
     busConfig.mosi_io_num = -1; // GPIO13 on SPI2_HOST; GPIO23 on SPI3_HOST
 #endif
-    busConfig.data0_io_num = -1;
 #ifdef RF24_DEFAULT_MISO
     busConfig.miso_io_num = RF24_DEFAULT_MISO;
 #elif defined(CONFIG_RF24_DEFAULT_MISO)
@@ -26,7 +26,6 @@ void SPIClass::begin(spi_host_device_t busNo, uint32_t speed)
 #else
     busConfig.miso_io_num = -1; // GPIO12 on SPI2_HOST; GPIO19 on SPI3_HOST
 #endif
-    busConfig.data1_io_num = -1;
 #ifdef RF24_DEFAULT_SCLK
     busConfig.sclk_io_num = RF24_DEFAULT_SCLK;
 #elif defined(CONFIG_RF24_DEFAULT_SCLK)
@@ -35,38 +34,29 @@ void SPIClass::begin(spi_host_device_t busNo, uint32_t speed)
     busConfig.sclk_io_num = -1; // GPIO14 on SPI2_HOST; GPIO18 on SPI3_HOST
 #endif
     busConfig.quadwp_io_num = -1;
-    busConfig.data2_io_num = -1;
     busConfig.quadhd_io_num = -1;
-    busConfig.data3_io_num = -1;
     busConfig.data4_io_num = -1;
     busConfig.data5_io_num = -1;
     busConfig.data6_io_num = -1;
     busConfig.data7_io_num = -1;
     busConfig.max_transfer_sz = 33; // RF24 lib only buffers 33 bytes for SPI transactions
-    busConfig.flags = 0;
-    busConfig.isr_cpu_id = ESP_INTR_CPU_AFFINITY_AUTO;
-    busConfig.intr_flags = 0;
 
     begin(busNo, speed, SPI_MODE0, &busConfig);
 }
 
 void SPIClass::begin(spi_host_device_t busNo, uint32_t speed, uint8_t mode, spi_bus_config_t* busConfig)
 {
-    esp_err_t ret = spi_bus_initialize(busNo, busConfig, SPI_DMA_CH_AUTO);
+    esp_err_t ret = spi_bus_initialize(busNo, busConfig, SPI_DMA_DISABLED);
     ESP_ERROR_CHECK(ret);
 
     spi_device_interface_config_t device_conf;
+    memset(&device_conf, 0, sizeof(device_conf));
     device_conf.mode = mode;
-    device_conf.clock_source = SPI_CLK_SRC_DEFAULT;
     device_conf.clock_speed_hz = speed;
-    device_conf.input_delay_ns = 0; // delay not needed between CSN assert and MISO start
     device_conf.spics_io_num = -1;  // RF24::csn() will control the CSN pin
-    device_conf.flags = SPI_DEVICE_NO_DUMMY;
     // we won't be using queued or interrupt-triggered SPI transactions.
     // disable those related features' config
-    device_conf.queue_size = 7;
-    device_conf.pre_cb = nullptr;
-    device_conf.post_cb = nullptr;
+    device_conf.queue_size = 1;
 
     ret = spi_bus_add_device(busNo, &device_conf, &bus);
     ESP_ERROR_CHECK(ret);
