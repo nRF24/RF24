nRF24/RF24

commit 0c34c0f8f36e03c27f8402b7ed2e4c97037722a8

Pipe0 fix (#1029)
* Pipe0 Fix

#1028

* Caches & restores RX_ADDR_P0 address when calling stopListening(). Ensures ACK functionality works on pipe 0.

* add docs bout cached TX address

---------

Co-authored-by: Brendan <2bndy5@gmail.com>

RF24.cpp
@@ -1188,6 +1188,7 @@ void RF24::stopListening(void)
         powerUp();
     }
 #endif
+    write_register(RX_ADDR_P0, pipe0_writing_address, addr_width);
     write_register(EN_RXADDR, static_cast<uint8_t>(read_register(EN_RXADDR) | _BV(pgm_read_byte(&child_pipe_enable[0])))); // Enable RX on pipe0
 }
 
@@ -1565,6 +1566,7 @@ void RF24::openWritingPipe(uint64_t value)
 
     write_register(RX_ADDR_P0, reinterpret_cast<uint8_t*>(&value), addr_width);
     write_register(TX_ADDR, reinterpret_cast<uint8_t*>(&value), addr_width);
+    memcpy(pipe0_writing_address, &value, addr_width);
 }
 
 /****************************************************************************/
@@ -1575,6 +1577,7 @@ void RF24::openWritingPipe(const uint8_t* address)
     // expects it LSB first too, so we're good.
     write_register(RX_ADDR_P0, address, addr_width);
     write_register(TX_ADDR, address, addr_width);
+    memcpy(pipe0_writing_address, address, addr_width);
 }
 
 /****************************************************************************/

commit d9417e0a54e20c0f7fa8adbd569162c53798c986

refactor: migrate to `stopListening(txAddress)` from `openWritingPipe()` (#1030)
follow up to #1029

`stopListening()` now writes the TX address to pipe 0 (for reading and writing). This deprecates the need for `openWritingPipe()` by offering an overloaded `stopListening(txAddress)`.

All examples (and the python wrapper) have been updated to use the new public `stopListening(txAddress)` accordingly.

The main take away is no duplicate SPI transactions when switching to TX mode and (re)setting the TX address. This aims to fix the performance regression in #1029.

---------

Co-authored-by: TMRh20 <tmrh20@gmail.com>

.github/workflows/doxygen.yml
@@ -50,5 +50,5 @@ jobs:
     uses: nRF24/.github/.github/workflows/build_docs.yaml@main
     with:
       deploy-gh-pages: ${{ github.event_name == 'release' || (github.event_name == 'workflow_dispatch' && github.ref == 'refs/heads/master') }}
-      doxygen-version: '1.12.0'
+      doxygen-version: '1.13.2'
     secrets: inherit

.readthedocs.yaml
@@ -15,7 +15,7 @@ build:
   commands:
     # Install doxygen from source distributions (conda forge does not keep up-to-date doxygen releases)
     - >
-      DOXYGEN_VERSION="1.12.0" &&
+      DOXYGEN_VERSION="1.13.2" &&
       mkdir .doxygen && cd .doxygen &&
       echo $(pwd) &&
       echo "https://sourceforge.net/projects/doxygen/files/rel-$DOXYGEN_VERSION/doxygen-$DOXYGEN_VERSION.linux.bin.tar.gz" &&

RF24.cpp
@@ -603,7 +603,6 @@ void RF24::_init_obj()
     _spi = &SPI;
 #endif // defined (RF24_SPI_PTR)
 
-    pipe0_reading_address[0] = 0;
     if (spi_speed <= 35000) { //Handle old BCM2835 speed constants, default to RF24_SPI_SPEED
         spi_speed = RF24_SPI_SPEED;
     }
@@ -1192,6 +1191,24 @@ void RF24::stopListening(void)
 
 /****************************************************************************/
 
+void RF24::stopListening(const uint64_t txAddress)
+{
+    memcpy(pipe0_writing_address, &txAddress, addr_width);
+    stopListening();
+    write_register(TX_ADDR, pipe0_writing_address, addr_width);
+}
+
+/****************************************************************************/
+
+void RF24::stopListening(const uint8_t* txAddress)
+{
+    memcpy(pipe0_writing_address, txAddress, addr_width);
+    stopListening();
+    write_register(TX_ADDR, pipe0_writing_address, addr_width);
+}
+
+/****************************************************************************/
+
 void RF24::powerDown(void)
 {
     ce(LOW); // Guarantee CE is low on powerDown
@@ -1626,11 +1643,13 @@ void RF24::openReadingPipe(uint8_t child, uint64_t address)
 
     if (child <= 5) {
         // For pipes 2-5, only write the LSB
-        if (child < 2) {
-            write_register(pgm_read_byte(&child_pipe[child]), reinterpret_cast<const uint8_t*>(&address), addr_width);
-        }
-        else {
+        if (child > 1) {
             write_register(pgm_read_byte(&child_pipe[child]), reinterpret_cast<const uint8_t*>(&address), 1);
+        }
+        // avoid overwriting the TX address on pipe 0 while still in TX mode.
+        // NOTE, the cached RX address on pipe 0 is written when startListening() is called.
+        else if (static_cast<bool>(config_reg & _BV(PRIM_RX)) || child != 0) {
+            write_register(pgm_read_byte(&child_pipe[child]), reinterpret_cast<const uint8_t*>(&address), addr_width);
         }
 
         // Note it would be more efficient to set all of the bits for all open
@@ -1668,11 +1687,13 @@ void RF24::openReadingPipe(uint8_t child, const uint8_t* address)
     }
     if (child <= 5) {
         // For pipes 2-5, only write the LSB
-        if (child < 2) {
-            write_register(pgm_read_byte(&child_pipe[child]), address, addr_width);
-        }
-        else {
+        if (child > 1) {
             write_register(pgm_read_byte(&child_pipe[child]), address, 1);
+        }
+        // avoid overwriting the TX address on pipe 0 while still in TX mode.
+        // NOTE, the cached RX address on pipe 0 is written when startListening() is called.
+        else if (static_cast<bool>(config_reg & _BV(PRIM_RX)) || child != 0) {
+            write_register(pgm_read_byte(&child_pipe[child]), address, addr_width);
         }
 
         // Note it would be more efficient to set all of the bits for all open
@@ -2037,6 +2058,11 @@ void RF24::stopConstCarrier()
     powerDown(); // per datasheet recommendation (just to be safe)
     write_register(RF_SETUP, static_cast<uint8_t>(read_register(RF_SETUP) & ~_BV(CONT_WAVE) & ~_BV(PLL_LOCK)));
     ce(LOW);
+    flush_tx();
+    if (isPVariant()) {
+        // restore the cached TX address
+        write_register(TX_ADDR, pipe0_writing_address, addr_width);
+    }
 }
 
 /****************************************************************************/

RF24.h
@@ -379,12 +379,15 @@ public:
      * @warning When the ACK payloads feature is enabled, the TX FIFO buffers are
      * flushed when calling this function. This is meant to discard any ACK
      * payloads that were not appended to acknowledgment packets.
-     *
-     * @note For auto-ack purposes, the TX address passed to openWritingPipe() will be restored to
-     * RX pipe 0. This still means that `stopListening()` shall be called before
-     * calling openWritingPipe() because the TX address is cached in openWritingPipe().
      */
     void stopListening(void);
+
+    /**
+     * @brief Similar to startListening(void) but changes the TX address.
+     * @param txAddress The new TX address.
+     * This value will be cached for auto-ack purposes.
+     */
+    void stopListening(const uint8_t* txAddress);
 
     /**
      * Check whether there are bytes available to be read
@@ -522,6 +525,8 @@ public:
     /**
      * New: Open a pipe for writing via byte array. Old addressing format retained
      * for compatibility.
+     *
+     * @deprecated Use `RF24::stopListening(uint8_t*)` instead.
      *
      * Only one writing pipe can be opened at once, but this function changes
      * the address that is used to transmit (ACK payloads/packets do not apply
@@ -2021,6 +2026,17 @@ public:
      * @endcode
      */
     void whatHappened(bool& tx_ok, bool& tx_fail, bool& rx_ready);
+
+    /**
+     * Similar to startListening(void) but changes the TX address.
+     *
+     * @deprecated Use stopListening(const uint8_t*) instead.
+     * See our [migration guide](migration.md) to understand what you should update in your code.
+     *
+     * @param txAddress The new TX address.
+     * This value will be cached for auto-ack purposes.
+     */
+    void stopListening(const uint64_t txAddress);
 
 private:
     /**@}*/

examples/AcknowledgementPayloads/AcknowledgementPayloads.ino
@@ -84,8 +84,8 @@ void setup() {
   // this feature for all nodes (TX & RX) to use ACK payloads.
   radio.enableAckPayload();
 
-  // set the TX address of the RX node into the TX pipe
-  radio.openWritingPipe(address[radioNumber]);  // always uses pipe 0
+  // set the TX address of the RX node for use on the TX pipe (pipe 0)
+  radio.stopListening(address[radioNumber]);  // put radio in TX mode
 
   // set the RX address of the TX node into a RX pipe
   radio.openReadingPipe(1, address[!radioNumber]);  // using pipe 1
@@ -93,9 +93,7 @@ void setup() {
   // additional setup specific to the node's role
   if (role) {
     // setup the TX payload
-
     memcpy(payload.message, "Hello ", 6);  // set the payload message
-    radio.stopListening();                 // put radio in TX mode
   } else {
     // setup the ACK payload & load the first response into the FIFO
 

examples/GettingStarted/GettingStarted.ino
@@ -74,16 +74,14 @@ void setup() {
   // number of bytes we need to transmit a float
   radio.setPayloadSize(sizeof(payload));  // float datatype occupies 4 bytes
 
-  // set the TX address of the RX node into the TX pipe
-  radio.openWritingPipe(address[radioNumber]);  // always uses pipe 0
+  // set the TX address of the RX node for use on the TX pipe (pipe 0)
+  radio.stopListening(address[radioNumber]);  // put radio in TX mode
 
   // set the RX address of the TX node into a RX pipe
   radio.openReadingPipe(1, address[!radioNumber]);  // using pipe 1
 
-  // additional setup specific to the node's role
-  if (role) {
-    radio.stopListening();  // put radio in TX mode
-  } else {
+  // additional setup specific to the node's RX role
+  if (!role) {
     radio.startListening();  // put radio in RX mode
   }
 

examples/InterruptConfigure/InterruptConfigure.ino
@@ -102,20 +102,15 @@ void setup() {
   // Acknowledgement packets have no payloads by default. We need to enable
   // this feature for all nodes (TX & RX) to use ACK payloads.
   radio.enableAckPayload();
-  // Fot this example, we use the same address to send data back and forth
 
-  // set the TX address of the RX node into the TX pipe
-  radio.openWritingPipe(address[radioNumber]);  // always uses pipe 0
+  // set the TX address of the RX node for use on the TX pipe (pipe 0)
+  radio.stopListening(address[radioNumber]);  // put radio in TX mode
 
   // set the RX address of the TX node into a RX pipe
   radio.openReadingPipe(1, address[!radioNumber]);  // using pipe 1
 
-  // additional setup specific to the node's role
-  if (role) {
-    // setup for TX mode
-    radio.stopListening();  // put radio in TX mode
-
-  } else {
+  // additional setup specific to the node's RX role
+  if (!role) {
     // setup for RX mode
 
     // let IRQ pin only trigger on "data_ready" event in RX mode

examples/ManualAcknowledgements/ManualAcknowledgements.ino
@@ -88,8 +88,8 @@ void setup() {
   // number of bytes we need to transmit a float
   radio.setPayloadSize(sizeof(payload));  // char[7] & uint8_t datatypes occupy 8 bytes
 
-  // set the TX address of the RX node into the TX pipe
-  radio.openWritingPipe(address[radioNumber]);  // always uses pipe 0
+  // set the TX address of the RX node for use on the TX pipe (pipe 0)
+  radio.stopListening(address[radioNumber]);  // put radio in TX mode
 
   // set the RX address of the TX node into a RX pipe
   radio.openReadingPipe(1, address[!radioNumber]);  // using pipe 1
@@ -98,7 +98,6 @@ void setup() {
     // setup the TX node
 
     memcpy(payload.message, "Hello ", 6);  // set the outgoing message
-    radio.stopListening();                 // put radio in TX mode
   } else {
     // setup the RX node
 

examples/MulticeiverDemo/MulticeiverDemo.ino
@@ -27,13 +27,13 @@ RF24 radio(CE_PIN, CSN_PIN);
 // an identifying device destination
 // Notice that the last byte is the only byte that changes in the last 5
 // addresses. This is a limitation of the nRF24L01 transceiver for pipes 2-5
-// because they use the same first 4 bytes from pipe 1.
-uint64_t address[6] = { 0x7878787878LL,
-                        0xB3B4B5B6F1LL,
-                        0xB3B4B5B6CDLL,
-                        0xB3B4B5B6A3LL,
-                        0xB3B4B5B60FLL,
-                        0xB3B4B5B605LL };
+// because they use the same first 4 MSBytes from pipe 1.
+uint8_t address[6][5] = { { 0x78, 0x78, 0x78, 0x78, 0x78 },
+                          { 0xF1, 0xB6, 0xB5, 0xB4, 0xB3 },
+                          { 0xCD, 0xB6, 0xB5, 0xB4, 0xB3 },
+                          { 0xA3, 0xB6, 0xB5, 0xB4, 0xB3 },
+                          { 0x0F, 0xB6, 0xB5, 0xB4, 0xB3 },
+                          { 0x05, 0xB6, 0xB5, 0xB4, 0xB3 } };
 
 // role variable is used to control whether this node is sending or receiving
 char role = 'R';  // integers 0-5 = TX node; character 'R' or integer 82 = RX node
@@ -183,9 +183,8 @@ void setRole() {
     payload.nodeID = role;
     payload.payloadID = 0;
 
-    // Set the address on pipe 0 to the RX node.
-    radio.stopListening();  // put radio in TX mode
-    radio.openWritingPipe(address[role]);
+    // set the TX address of the RX node for use on the TX pipe (pipe 0)
+    radio.stopListening(address[role]);  // put radio in TX mode
 
     // According to the datasheet, the auto-retry features's delay value should
     // be "skewed" to allow the RX node to receive 1 transmission at a time.

examples/StreamingData/StreamingData.ino
@@ -80,16 +80,14 @@ void setup() {
   // number of bytes we need to transmit
   radio.setPayloadSize(SIZE);  // default value is the maximum 32 bytes
 
-  // set the TX address of the RX node into the TX pipe
-  radio.openWritingPipe(address[radioNumber]);  // always uses pipe 0
+  // set the TX address of the RX node for use on the TX pipe (pipe 0)
+  radio.stopListening(address[radioNumber]);  // put radio in TX mode
 
   // set the RX address of the TX node into a RX pipe
   radio.openReadingPipe(1, address[!radioNumber]);  // using pipe 1
 
-  // additional setup specific to the node's role
-  if (role) {
-    radio.stopListening();  // put radio in TX mode
-  } else {
+  // additional setup specific to the node's RX role
+  if (!role) {
     radio.startListening();  // put radio in RX mode
   }
 

examples_linux/acknowledgementPayloads.cpp
@@ -96,8 +96,8 @@ int main(int argc, char** argv)
     // each other.
     radio.setPALevel(RF24_PA_LOW); // RF24_PA_MAX is default.
 
-    // set the TX address of the RX node into the TX pipe
-    radio.openWritingPipe(address[radioNumber]); // always uses pipe 0
+    // set the TX address of the RX node for use on the TX pipe (pipe 0)
+    radio.stopListening(address[radioNumber]);
 
     // set the RX address of the TX node into a RX pipe
     radio.openReadingPipe(1, address[!radioNumber]); // using pipe 1

examples_linux/acknowledgement_payloads.py
@@ -51,8 +51,8 @@ radio.enableAckPayload()
 # usually run with nRF24L01 transceivers in close proximity of each other
 radio.setPALevel(RF24_PA_LOW)  # RF24_PA_MAX is default
 
-# set the TX address of the RX node into the TX pipe
-radio.openWritingPipe(address[radio_number])  # always uses pipe 0
+# set the TX address of the RX node for use on the TX pipe (pipe 0)
+radio.stopListening(address[radio_number])
 
 # set the RX address of the TX node into a RX pipe
 radio.openReadingPipe(1, address[not radio_number])  # using pipe 1

examples_linux/getting_started.py
@@ -48,8 +48,8 @@ radio_number = bool(
 # usually run with nRF24L01 transceivers in close proximity of each other
 radio.setPALevel(RF24_PA_LOW)  # RF24_PA_MAX is default
 
-# set the TX address of the RX node into the TX pipe
-radio.openWritingPipe(address[radio_number])  # always uses pipe 0
+# set the TX address of the RX node for use on the TX pipe (pipe 0)
+radio.stopListening(address[radio_number])
 
 # set the RX address of the TX node into a RX pipe
 radio.openReadingPipe(1, address[not radio_number])  # using pipe 1

examples_linux/gettingstarted.cpp
@@ -87,8 +87,8 @@ int main(int argc, char** argv)
     // each other.
     radio.setPALevel(RF24_PA_LOW); // RF24_PA_MAX is default.
 
-    // set the TX address of the RX node into the TX pipe
-    radio.openWritingPipe(address[radioNumber]); // always uses pipe 0
+    // set the TX address of the RX node for use on the TX pipe (pipe 0)
+    radio.stopListening(address[radioNumber]);
 
     // set the RX address of the TX node into a RX pipe
     radio.openReadingPipe(1, address[!radioNumber]); // using pipe 1

examples_linux/interruptConfigure.cpp
@@ -109,8 +109,8 @@ int main(int argc, char** argv)
     // each other.
     radio.setPALevel(RF24_PA_LOW); // RF24_PA_MAX is default.
 
-    // set the TX address of the RX node into the TX pipe
-    radio.openWritingPipe(address[radioNumber]); // always uses pipe 0
+    // set the TX address of the RX node for use on the TX pipe (pipe 0)
+    radio.stopListening(address[radioNumber]);
 
     // set the RX address of the TX node into a RX pipe
     radio.openReadingPipe(1, address[!radioNumber]); // using pipe 1

examples_linux/interrupt_configure.py
@@ -82,8 +82,8 @@ radio.enableAckPayload()  # enable ACK payloads
 # usually run with nRF24L01 transceivers in close proximity of each other
 radio.setPALevel(RF24_PA_LOW)  # RF24_PA_MAX is default
 
-# set the TX address of the RX node into the TX pipe
-radio.openWritingPipe(address[radio_number])  # always uses pipe 0
+# set the TX address of the RX node for use on the TX pipe (pipe 0)
+radio.stopListening(address[radio_number])
 
 # set the RX address of the TX node into a RX pipe
 radio.openReadingPipe(1, address[not radio_number])  # using pipe 1

examples_linux/manualAcknowledgements.cpp
@@ -103,8 +103,8 @@ int main(int argc, char** argv)
     // number of bytes we need to transmit a float
     radio.setPayloadSize(sizeof(payload)); // char[7] & uint8_t datatypes occupy 8 bytes
 
-    // set the TX address of the RX node into the TX pipe
-    radio.openWritingPipe(address[radioNumber]); // always uses pipe 0
+    // set the TX address of the RX node for use on the TX pipe (pipe 0)
+    radio.stopListening(address[radioNumber]);
 
     // set the RX address of the TX node into a RX pipe
     radio.openReadingPipe(1, address[!radioNumber]); // using pipe 1

examples_linux/manual_acknowledgements.py
@@ -54,8 +54,8 @@ radio_number = bool(
 # usually run with nRF24L01 transceivers in close proximity of each other
 radio.setPALevel(RF24_PA_LOW)  # RF24_PA_MAX is default
 
-# set the TX address of the RX node into the TX pipe
-radio.openWritingPipe(address[radio_number])  # always uses pipe 0
+# set the TX address of the RX node for use on the TX pipe (pipe 0)
+radio.stopListening(address[radio_number])
 
 # set the RX address of the TX node into a RX pipe
 radio.openReadingPipe(1, address[not radio_number])  # using pipe 1

examples_linux/multiceiverDemo.cpp
@@ -47,13 +47,13 @@ RF24 radio(CE_PIN, CSN_PIN);
 // an identifying device destination
 // Notice that the last byte is the only byte that changes in the last 5
 // addresses. This is a limitation of the nRF24L01 transceiver for pipes 2-5
-// because they use the same first 4 bytes from pipe 1.
-uint64_t address[6] = {0x7878787878LL,
-                       0xB3B4B5B6F1LL,
-                       0xB3B4B5B6CDLL,
-                       0xB3B4B5B6A3LL,
-                       0xB3B4B5B60FLL,
-                       0xB3B4B5B605LL};
+// because they use the same first 4 MSBytes from pipe 1.
+uint8_t address[6][5] = {{0x78, 0x78, 0x78, 0x78, 0x78},
+                         {0xF1, 0xB6, 0xB5, 0xB4, 0xB3},
+                         {0xCD, 0xB6, 0xB5, 0xB4, 0xB3},
+                         {0xA3, 0xB6, 0xB5, 0xB4, 0xB3},
+                         {0x0F, 0xB6, 0xB5, 0xB4, 0xB3},
+                         {0x05, 0xB6, 0xB5, 0xB4, 0xB3}};
 
 // For this example, we'll be using a payload containing
 // a node ID number and a single integer number that will be incremented
@@ -181,9 +181,8 @@ void master(unsigned int role)
     payload.nodeID = role;
     payload.payloadID = 0;
 
-    // Set the address on pipe 0 to the RX node.
-    radio.stopListening(); // put radio in TX mode
-    radio.openWritingPipe(address[role]);
+    // set the TX address of the RX node for use on the TX pipe (pipe 0)
+    radio.stopListening(address[role]); // put radio in TX mode
 
     // According to the datasheet, the auto-retry features's delay value should
     // be "skewed" to allow the RX node to receive 1 transmission at a time.

examples_linux/multiceiver_demo.py
@@ -74,9 +74,9 @@ def master(node_number):
         ((node_number * 3) % 12) + 3, 15
     )  # maximum value is 15 for both args
 
-    radio.stopListening()  # put radio in TX mode
-    # set the TX address to the address of the base station.
-    radio.openWritingPipe(addresses[node_number])
+    # set the TX address of the RX node for use on the TX pipe (pipe 0)
+    radio.stopListening(addresses[node_number])  # put radio in TX mode
+
     counter = 0
     failures = 0
     while failures < 6:

examples_linux/streamingData.cpp
@@ -149,8 +149,8 @@ int main(int argc, char** argv)
     // each other.
     radio.setPALevel(RF24_PA_LOW); // RF24_PA_MAX is default.
 
-    // set the TX address of the RX node into the TX pipe
-    radio.openWritingPipe(address[radioNumber]); // always uses pipe 0
+    // set the TX address of the RX node for use on the TX pipe (pipe 0)
+    radio.stopListening(address[radioNumber]);
 
     // set the RX address of the TX node into a RX pipe
     radio.openReadingPipe(1, address[!radioNumber]); // using pipe 1

examples_linux/streaming_data.py
@@ -47,8 +47,8 @@ radio_number = bool(
 # usually run with nRF24L01 transceivers in close proximity of each other
 radio.setPALevel(RF24_PA_LOW)  # RF24_PA_MAX is default
 
-# set the TX address of the RX node into the TX pipe
-radio.openWritingPipe(address[radio_number])  # always uses pipe 0
+# set the TX address of the RX node for use on the TX pipe (pipe 0)
+radio.stopListening(address[radio_number])
 
 # set the RX address of the TX node into a RX pipe
 radio.openReadingPipe(1, address[not radio_number])  # using pipe 1

examples_pico/acknowledgementPayloads.cpp
@@ -78,8 +78,8 @@ bool setup()
     // this feature for all nodes (TX & RX) to use ACK payloads.
     radio.enableAckPayload();
 
-    // set the TX address of the RX node into the TX pipe
-    radio.openWritingPipe(address[radioNumber]); // always uses pipe 0
+    // set the TX address of the RX node for use on the TX pipe (pipe 0)
+    radio.stopListening(address[radioNumber]);
 
     // set the RX address of the TX node into a RX pipe
     radio.openReadingPipe(1, address[!radioNumber]); // using pipe 1

examples_pico/gettingStarted.cpp
@@ -67,8 +67,8 @@ bool setup()
     // number of bytes we need to transmit a float
     radio.setPayloadSize(sizeof(payload)); // float datatype occupies 4 bytes
 
-    // set the TX address of the RX node into the TX pipe
-    radio.openWritingPipe(address[radioNumber]); // always uses pipe 0
+    // set the TX address of the RX node for use on the TX pipe (pipe 0)
+    radio.stopListening(address[radioNumber]);
 
     // set the RX address of the TX node into a RX pipe
     radio.openReadingPipe(1, address[!radioNumber]); // using pipe 1

examples_pico/interruptConfigure.cpp
@@ -97,18 +97,14 @@ bool setup()
     radio.enableAckPayload();
     // Fot this example, we use the same address to send data back and forth
 
-    // set the TX address of the RX node into the TX pipe
-    radio.openWritingPipe(address[radioNumber]); // always uses pipe 0
+    // set the TX address of the RX node for use on the TX pipe (pipe 0)
+    radio.stopListening(address[radioNumber]);
 
     // set the RX address of the TX node into a RX pipe
     radio.openReadingPipe(1, address[!radioNumber]); // using pipe 1
 
     // additional setup specific to the node's role
-    if (role) {
-        // setup for TX mode
-        radio.stopListening(); // put radio in TX mode
-    }
-    else {
+    if (!role) {
         // setup for RX mode
 
         // disable IRQ pin in RX mode

examples_pico/manualAcknowledgements.cpp
@@ -82,8 +82,8 @@ bool setup()
     // number of bytes we need to transmit a float
     radio.setPayloadSize(sizeof(payload)); // char[7] & uint8_t datatypes occupy 8 bytes
 
-    // set the TX address of the RX node into the TX pipe
-    radio.openWritingPipe(address[radioNumber]); // always uses pipe 0
+    // set the TX address of the RX node for use on the TX pipe (pipe 0)
+    radio.stopListening(address[radioNumber]);
 
     // set the RX address of the TX node into a RX pipe
     radio.openReadingPipe(1, address[!radioNumber]); // using pipe 1
@@ -92,7 +92,6 @@ bool setup()
         // setup the TX node
 
         memcpy(payload.message, "Hello ", 6); // set the outgoing message
-        radio.stopListening();                // put radio in TX mode
     }
     else {
         // setup the RX node

examples_pico/multiceiverDemo.cpp
@@ -27,13 +27,13 @@ RF24 radio(CE_PIN, CSN_PIN);
 // an identifying device destination
 // Notice that the last byte is the only byte that changes in the last 5
 // addresses. This is a limitation of the nRF24L01 transceiver for pipes 2-5
-// because they use the same first 4 bytes from pipe 1.
-uint64_t address[6] = {0x7878787878LL,
-                       0xB3B4B5B6F1LL,
-                       0xB3B4B5B6CDLL,
-                       0xB3B4B5B6A3LL,
-                       0xB3B4B5B60FLL,
-                       0xB3B4B5B605LL};
+// because they use the same first 4 MSBytes from pipe 1.
+uint8_t address[6][5] = {{0x78, 0x78, 0x78, 0x78, 0x78},
+                         {0xF1, 0xB6, 0xB5, 0xB4, 0xB3},
+                         {0xCD, 0xB6, 0xB5, 0xB4, 0xB3},
+                         {0xA3, 0xB6, 0xB5, 0xB4, 0xB3},
+                         {0x0F, 0xB6, 0xB5, 0xB4, 0xB3},
+                         {0x05, 0xB6, 0xB5, 0xB4, 0xB3}};
 
 // Because this example allow up to 6 nodes (specified by numbers 0-5) to
 // transmit and only 1 node to receive, we will use a negative value in our
@@ -190,9 +190,8 @@ void setRole()
         payload.nodeID = role;
         payload.payloadID = 0;
 
-        // Set the address on pipe 0 to the RX node.
-        radio.stopListening(); // put radio in TX mode
-        radio.openWritingPipe(address[role]);
+        // set the TX address of the RX node for use on the TX pipe (pipe 0)
+        radio.stopListening(address[role]); // put radio in TX mode
 
         // According to the datasheet, the auto-retry features's delay value should
         // be "skewed" to allow the RX node to receive 1 transmission at a time.

examples_pico/streamingData.cpp
@@ -73,17 +73,14 @@ bool setup()
     // number of bytes we need to transmit
     radio.setPayloadSize(SIZE); // default value is the maximum 32 bytes
 
-    // set the TX address of the RX node into the TX pipe
-    radio.openWritingPipe(address[radioNumber]); // always uses pipe 0
+    // set the TX address of the RX node for use on the TX pipe (pipe 0)
+    radio.stopListening(address[radioNumber]);
 
     // set the RX address of the TX node into a RX pipe
     radio.openReadingPipe(1, address[!radioNumber]); // using pipe 1
 
     // additional setup specific to the node's role
-    if (role) {
-        radio.stopListening(); // put radio in TX mode
-    }
-    else {
+    if (!role) {
         radio.startListening(); // put radio in RX mode
     }
 

pyRF24/pyRF24.cpp
@@ -98,6 +98,11 @@ void openWritingPipe_wrap(RF24& ref, const bp::object address)
     ref.openWritingPipe((const uint8_t*)(get_bytes_or_bytearray_str(address)));
 }
 
+void stopListening_wrap(RF24& ref, const bp::object address)
+{
+    ref.stopListening((const uint8_t*)(get_bytes_or_bytearray_str(address)));
+}
+
 void openReadingPipe_wrap(RF24& ref, uint8_t number, const bp::object address)
 {
     ref.openReadingPipe(number, (const uint8_t*)(get_bytes_or_bytearray_str(address)));
@@ -351,7 +356,8 @@ BOOST_PYTHON_MODULE(RF24)
         .def("startFastWrite", &startFastWrite_wrap2, (bp::arg("buf"), bp::arg("len"), bp::arg("multicast"), bp::arg("startTx")))
         .def("startListening", &RF24::startListening)
         .def("startWrite", &startWrite_wrap, (bp::arg("buf"), bp::arg("len"), bp::arg("multicast")))
-        .def("stopListening", &RF24::stopListening)
+        .def("stopListening", (void(::RF24::*)(void))(&RF24::stopListening))
+        .def("stopListening", &stopListening_wrap, (bp::arg("txAddress")))
         .def("testCarrier", &RF24::testCarrier)
         .def("testRPD", &RF24::testRPD)
         .def("toggleAllPipes", &RF24::toggleAllPipes)