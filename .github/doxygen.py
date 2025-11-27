a87bc69f195f0d228d2fed943077f6243f1f9456
Brendan <2bndy5@gmail.com>
2025-11-27 02:00:55 -0800

    chore(dev): revise CI (#1050)
    corresponding to nRF24/.github#23
    
    * auto-cancel CI runs when a new run is queued
      does not apply to events on master branch
    * use LF everywhere
    * use doxygen v1.15.0 and adjust path to images and some other MD lint rules

--- .github/doxygen.py
+++ .github/doxygen.py
@@ -4,6 +4,7 @@ This script is meant to be execute in CI only.
 Running this locally will alter the Doxyfile.
 Such changes SHOULD NOT be pushed to the remote.
 """
+
 from pathlib import Path
 import json
 
@@ -17,9 +18,12 @@ def overwrite_doxygen_value():
     properties = json.loads(PROPERTIES.read_text(encoding="utf-8"))
     assert "version" in properties
     config = CONFIG.read_text(encoding="utf-8")
-    TMP.write_text("PROJECT_NUMBER = {}\n".format(properties["version"]), encoding="utf-8")
+    TMP.write_text(
+        "PROJECT_NUMBER = {}\n".format(properties["version"]), encoding="utf-8"
+    )
     config += f"\n@INCLUDE = {str(TMP)}\n"
     CONFIG.write_text(config, encoding="utf-8")
+
 
 if __name__ == "__main__":
     overwrite_doxygen_value()
