e5c622942d8123f6964674159e7cb14b56eaadf8
TMRh20 <tmrh20@gmail.com>
2026-03-26 04:46:23 -0600

    Disable interrupts by default
    - Found there is some sort of instability related to interrupts being fully enabled in the radio itself
    - Disable interrupts by default in RF24
    
    https://github.com/nRF24/RF24/issues/1061

--- RF24.cpp
+++ RF24.cpp
@@ -1116,13 +1116,13 @@ bool RF24::_init_radio()
     //      16-bit CRC (CRC required by auto-ack)
     // Do not write CE high so radio will remain in standby I mode
     // PTX should use only 22uA of power
-    write_register(NRF_CONFIG, (_BV(EN_CRC) | _BV(CRCO)));
+    write_register(NRF_CONFIG, (_BV(EN_CRC) | _BV(CRCO) | _BV(MASK_RX_DR) | _BV(MASK_TX_DS) | _BV(MASK_MAX_RT));
     config_reg = read_register(NRF_CONFIG);
 
     powerUp();
 
     // if config is not set correctly then there was a bad response from module
-    return config_reg == (_BV(EN_CRC) | _BV(CRCO) | _BV(PWR_UP)) ? true : false;
+    return config_reg == (_BV(EN_CRC) | _BV(CRCO) | _BV(PWR_UP) | _BV(MASK_RX_DR) | _BV(MASK_TX_DS) | _BV(MASK_MAX_RT)) ? true : false;
 }
 
 /****************************************************************************/
